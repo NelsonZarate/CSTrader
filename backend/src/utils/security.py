9bd42afc453aa3047aa3283028d31ef9bbeaa1ac
Nelson Zarate <nelsonpinto1415@gmail.com>
2025-11-07 18:09:05 +0000

    Add security module for user authentication with token decoding

---
+++ backend/src/utils/security.py
@@ -0,0 +1,10 @@
+from fastapi.security import HTTPBearer, HTTPAuthorizationCredentials
+from fastapi import Depends
+from src.utils.auth_utils import decode_access_token
+
+security = HTTPBearer()
+
+def get_current_user(credentials: HTTPAuthorizationCredentials = Depends(security)):
+    token = credentials.credentials
+    payload = decode_access_token(token)
+    return payload  # pode retornar o email, role, etc.
